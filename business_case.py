a708f7ed2dff8e6d4c86003bb497831742712a24
Chawaissalman <69862349+Chawaissalman@users.noreply.github.com>
2026-04-14 10:36:15 +0200

    Update business_case.py

--- business_case.py
+++ business_case.py
@@ -12,7 +12,7 @@ import plotly.express as px
 import pandas as pd
 import numpy as np
 
-from pages.core_engine import (
+from engine.core_engine import (
     ARCHITECTURES, CLIMATES, REGULATIONS,
     run_thermo_model, run_financial_model,
     score_business_cases, BusinessScenario,
